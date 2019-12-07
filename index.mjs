5305fef596a7274c83dc9dca796e7fb8d6e5a0ef
SachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-12-07 22:07:00 +0530

    Refactor and create basic structure of app

--- index.mjs
+++ index.mjs
@@ -5,39 +5,12 @@
  * @author SachchidanandY
 **/
 import express from 'express';
-import passport from 'passport';
-import passportGoogleOauth20 from 'passport-google-oauth20';
-import { GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    GOOGLE_OAUTH_CALLBACK_URL,
-    ENV,
-    PORT
-} from './lib/config';
-
+import { ENV, PORT } from './lib/config';
+import routers from './lib/routes';
 const app = express();
-const Strategy = passportGoogleOauth20.Strategy;
 
-//Bind Google OAuth strategy with passport
-passport.use(
-    new Strategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: GOOGLE_OAUTH_CALLBACK_URL
-    }, (accessTocken, refreshToken, profile, done) => {
-        console.log('Access Tocken = ', accessTocken);
-        console.log('refreshToken = ', refreshToken);
-        console.log('profile = ', profile);
-        console.log('Access done = ', done);
-    })
-);
-
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-    })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+// Register Routes
+routers(app);
 
 app.listen(PORT, () => {
     console.log(`Server running at port : ${PORT} in ${ENV} environment`);
