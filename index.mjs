ca3dfe4db038f886c6fdc43e5ed06188e0799f8e
SachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-12-08 18:06:08 +0530

    Connect to mongoDB

--- index.mjs
+++ index.mjs
@@ -7,7 +7,12 @@
 import express from 'express';
 import { ENV, PORT } from './lib/config';
 import routers from './lib/routes';
+import connectDB from './lib/config/connectDB';
+
 const app = express();
+
+// Connect to mongoDB
+connectDB();
 
 // Register Routes
 routers(app);
