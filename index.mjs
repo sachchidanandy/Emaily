2a12e3f97102ec06a846b33ca67421127d316045
SachchidanandY <sachchidanandy@mindfiresolutions.com>
2019-12-30 14:31:05 +0530

    Create auth middleware and get user info api

--- index.mjs
+++ index.mjs
@@ -8,6 +8,8 @@ import express from 'express';
 import { ENV, PORT } from './lib/config';
 import routers from './lib/routes';
 import connectDB from './lib/config/connectDB';
+import { INTERNAL_SERVER_ERROR } from './lib/constants/statusCode';
+import { SOMETHING_BROKE } from './lib/constants/errorResponse';
 
 const app = express();
 
@@ -15,7 +17,13 @@ const app = express();
 connectDB();
 
 // Register Routes
-routers(app);
+app.use('/api', routers);
+
+// Add error handler middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(INTERNAL_SERVER_ERROR).json({SOMETHING_BROKE});
+});
 
 app.listen(PORT, () => {
     console.log(`Server running at port : ${PORT} in ${ENV} environment`);
