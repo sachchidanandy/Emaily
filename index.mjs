b45e715e248637a799d08cf3eb7fbe282e0854fc
SachchidanandY <sachchidanandy@mindfiresolutions.com>
2020-01-06 20:47:58 +0530

    Add socket based auth

--- index.mjs
+++ index.mjs
@@ -5,19 +5,38 @@
  * @author SachchidanandY
 **/
 import express from 'express';
-import { ENV, PORT } from './lib/config';
+import http from 'http';
+import { ENV, PORT, SESSION_SECRET } from './lib/config';
 import routers from './lib/routes';
 import connectDB from './lib/config/connectDB';
 import { INTERNAL_SERVER_ERROR } from './lib/constants/statusCode';
 import { SOMETHING_BROKE } from './lib/constants/errorResponse';
+import socketio from 'socket.io';
+import session from 'express-session';
 
 const app = express();
 
 // Connect to mongoDB
 connectDB();
 
+// Create httpServer
+const httpServer = http.createServer(app);
+
+// Connecting sockets to the server and adding them to the request
+// so that we can access them later in the controller
+const io = socketio(httpServer)
+app.set('io', io)
+
+// saveUninitialized: true allows us to attach the socket id to the session
+// before we have athenticated the user
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+
 // Register Routes
-app.use('/api', routers);
+app.use('/api',routers);
 
 // Add error handler middleware
 app.use((err, req, res, next) => {
@@ -25,6 +44,6 @@ app.use((err, req, res, next) => {
     res.status(INTERNAL_SERVER_ERROR).json({SOMETHING_BROKE});
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server running at port : ${PORT} in ${ENV} environment`);
 });
